5e004b5f0ca7c248e637b17b391075407a205dc0
Alberto Arias <alberto@boodland.com>
2018-01-31 16:38:06 +0100

    Add Vuetify beta version CSS Framework

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import { Vuetify, VApp } from 'vuetify';
+
 import App from './App';
 import router from './router';
 
+require('vuetify/src/stylus/app.styl');
+
+
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+  },
+});
 
 /* eslint-disable no-new */
 new Vue({
