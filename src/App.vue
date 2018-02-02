e3577a9128021d94f50b82a54283ea9c30099c7c
Alberto Arias <alberto@boodland.com>
2018-02-02 14:12:15 +0100

    Create first version of NavbarHeader and use it within App component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
 <template>
   <div id="app">
     <v-app>
-      <v-toolbar dark color="primary">
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title class="white--text">Subject Hub</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>search</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>apps</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>refresh</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>more</v-icon>
-        </v-btn>
-      </v-toolbar>
+      <navbar-header></navbar-header>
       <main>
         <img src="./assets/logo.png" alt="Vue.js PWA">
         <router-view></router-view>
@@ -27,8 +11,13 @@
 </template>
 
 <script>
+import NavbarHeader from './components/navigation/NavbarHeader';
+
 export default {
   name: 'app',
+  components: {
+    'navbar-header': NavbarHeader,
+  },
 };
 </script>
 
---
+++ src/components/navigation/NavbarHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar dark color="primary">
+    <v-layout justify-space-between="true" align-center="true">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text mx-0">{{title}}</v-toolbar-title>
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+    </v-layout>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    name: 'navbar-header',
+    data() {
+      return {
+        title: 'Subject Hub',
+      };
+    },
+  };
+</script>
