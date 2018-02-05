54ede5a42f73c09eaaf0bef37eb65560c6dd95eb
Alberto Arias <alberto@boodland.com>
2018-02-05 14:22:19 +0100

    Create first version of NavbarList component

---
+++ src/components/navigation/NavbarList.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-navigation-drawer
+    temporary
+    v-model="drawer"
+    absolute
+    width="200"
+  >
+    <v-list dense class="mt-2">
+      <v-list-tile v-for="item in items" :key="item.title" @click="">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Dashboard', icon: 'apps' },
+          { title: 'Subjects', icon: 'school' },
+          { title: 'Tutors', icon: 'person' },
+          { title: 'Exams', icon: 'edit' },
+        ],
+      };
+    },
+  };
+</script>
