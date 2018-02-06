ad51bd91dbc90227d0b08298d01435248828c686
Alberto Arias <alberto@boodland.com>
2018-02-06 19:38:08 +0100

    Display list when menu icon is clicked

--- src/components/navigation/AppNavbar.vue
+++ src/components/navigation/AppNavbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-once>
-    <navbar-list app></navbar-list>
-    <navbar-header app></navbar-header>
+  <div>
+    <navbar-list app :displayList.sync="isListDisplayed"></navbar-list>
+    <navbar-header app @menuIconClicked="displayListHandler"></navbar-header>
   </div>
 </template>
 
@@ -14,6 +14,16 @@
     components: {
       NavbarList,
       NavbarHeader,
+    },
+    data() {
+      return {
+        isListDisplayed: false,
+      };
+    },
+    methods: {
+      displayListHandler() {
+        this.isListDisplayed = true;
+      },
     },
   };
 </script>
