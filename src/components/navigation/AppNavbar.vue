e9f864ea64e67e349a0b9474a33d9b8a7cfe0922
Alberto Arias <alberto@boodland.com>
2018-02-06 16:21:41 +0100

    Create brand new AppNavbar component

---
+++ src/components/navigation/AppNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-once>
+    <navbar-list app></navbar-list>
+    <navbar-header app></navbar-header>
+  </div>
+</template>
+
+<script>
+  import NavbarHeader from './NavbarHeader';
+  import NavbarList from './NavbarList';
+
+  export default {
+    name: 'app-navbar',
+    components: {
+      NavbarList,
+      NavbarHeader,
+    },
+  };
+</script>
