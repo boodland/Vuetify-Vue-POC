ac0f4434b92184dfb8789750ff900c26a23c7837
Alberto Arias <alberto@boodland.com>
2018-02-09 19:07:23 +0100

    Add navigation links to the navigation list

--- src/components/navigation/NavbarList.vue
+++ src/components/navigation/NavbarList.vue
@@ -7,7 +7,7 @@
     @click.native="hideDrawerHandler"
   >
     <v-list dense class="mt-2">
-      <v-list-tile v-for="item in items" :key="item.title" @click="hideDrawerHandler">
+      <v-list-tile v-for="item in items" :key="item.title" @click="hideDrawerHandler" :to="item.link">
         <v-list-tile-action>
           <v-icon>{{ item.icon }}</v-icon>
         </v-list-tile-action>
@@ -27,10 +27,10 @@
       return {
         showDrawer: this.value,
         items: [
-          { title: 'Dashboard', icon: 'apps' },
-          { title: 'Subjects', icon: 'school' },
-          { title: 'Tutors', icon: 'person' },
-          { title: 'Exams', icon: 'edit' },
+          { title: 'Dashboard', link: '/dashboard', icon: 'apps' },
+          { title: 'Subjects', link: '/subjects', icon: 'school' },
+          { title: 'Tutors', link: '/tutors', icon: 'person' },
+          { title: 'Exams', link: '/exams', icon: 'edit' },
         ],
       };
     },
