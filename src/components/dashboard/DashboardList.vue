ba2765edf9ec4b20d22382cb2a2669974d96d6a2
Alberto Arias <alberto@boodland.com>
2018-02-09 16:24:25 +0100

    Move DashboardListItem to its own component

--- src/components/dashboard/DashboardList.vue
+++ src/components/dashboard/DashboardList.vue
@@ -2,29 +2,7 @@
 <v-container grid-list-md>
   <v-layout row wrap>
     <v-flex xs6 v-for="item in 4" :key="item">
-    <v-card>
-      <v-card-title class="pt-1 pb-3">
-        <div>
-          <h2>HISTORY</h2>
-          <v-icon small>event</v-icon>
-          <span class="card-subtitle">May 1, 2018</span>
-        </div>
-      </v-card-title>
-
-      <v-card-media
-        height="130"
-        src="/static/img/history.jpg"
-      >
-        <v-layout class="text-xs-center" row wrap justify-center align-center>
-          <div class="card-seats mt-3 mx-2">680 SEATS</div>
-          <v-btn class="mb-2">View Subject</v-btn>
-        </v-layout>
-      </v-card-media>
-      <v-card-actions>
-        <v-btn flat color="orange">Share</v-btn>
-        <v-btn flat color="orange">Explore</v-btn>
-      </v-card-actions>
-    </v-card>
+      <dashboard-list-item></dashboard-list-item>
     </v-flex>
   </v-layout>
 </v-container>
@@ -32,8 +10,13 @@
 
 
 <script>
+  import DashboardListItem from './DashboardListItem';
+
   export default {
     name: 'dashboard-list',
+    components: {
+      DashboardListItem,
+    },
     data() {
       return {
         dashboardItems: [],
@@ -42,20 +25,3 @@
   };
 </script>
 
-
-<style scoped>
-
-  .card-subtitle {
-    color: rgba(0,0,0,.54);
-    font-size: 16px;
-    vertical-align: middle;
-  }
-
-  .card-seats {
-    width: 100%;
-    font-weight: bold;
-    font-size: 30px;
-    background-color: rgba(255, 255, 255, 0.7);
-  }
-
-</style>
---
+++ src/components/dashboard/DashboardListItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card>
+    <v-card-title class="pt-1 pb-3">
+      <div>
+        <h2>HISTORY</h2>
+        <v-icon small>event</v-icon>
+        <span class="card-subtitle">May 1, 2018</span>
+      </div>
+    </v-card-title>
+
+    <v-card-media
+      height="130"
+      src="/static/img/history.jpg"
+    >
+      <v-layout class="text-xs-center" row wrap justify-center align-center>
+        <div class="card-seats mt-3 mx-2">680 SEATS</div>
+        <v-btn class="mb-2">View Subject</v-btn>
+      </v-layout>
+    </v-card-media>
+    <v-card-actions>
+      <v-btn flat color="orange">Share</v-btn>
+      <v-btn flat color="orange">Explore</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+
+<script>
+  export default {
+    name: 'dashboard-list-item',
+  };
+</script>
+
+<style scoped>
+
+  .card-subtitle {
+    color: rgba(0,0,0,.54);
+    font-size: 16px;
+    vertical-align: middle;
+  }
+
+  .card-seats {
+    width: 100%;
+    font-weight: bold;
+    font-size: 30px;
+    background-color: rgba(255, 255, 255, 0.7);
+  }
+
+</style>
