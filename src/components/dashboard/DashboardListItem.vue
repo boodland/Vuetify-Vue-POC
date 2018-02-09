35dc645f8efd3f98d26690f4278190b94452493e
Alberto Arias <alberto@boodland.com>
2018-02-09 17:48:51 +0100

    Add props to component and use them to render the component Add action to each DashboardListItem

--- src/components/dashboard/DashboardListItem.vue
+++ src/components/dashboard/DashboardListItem.vue
@@ -2,24 +2,26 @@
   <v-card>
     <v-card-title class="pt-1 pb-3">
       <div>
-        <h2>HISTORY</h2>
+        <h2>{{title.toUpperCase()}}</h2>
         <v-icon small>event</v-icon>
-        <span class="card-subtitle">May 1, 2018</span>
+        <span class="card-subtitle">{{assessmentDate.toDateString()}}</span>
       </div>
     </v-card-title>
 
     <v-card-media
       height="130"
-      src="/static/img/history.jpg"
+      :src="'/static/img/' + image + '.jpg'"
     >
       <v-layout class="text-xs-center" row wrap justify-center align-center>
-        <div class="card-seats mt-3 mx-2">680 SEATS</div>
+        <div class="card-seats mt-3 mx-2">{{numOfSeats}} SEATS</div>
         <v-btn class="mb-2">View Subject</v-btn>
       </v-layout>
     </v-card-media>
     <v-card-actions>
-      <v-btn flat color="orange">Share</v-btn>
-      <v-btn flat color="orange">Explore</v-btn>
+      <v-layout class="card-actions">
+        <v-chip color="primary" text-color="white">{{numOfExams}} Exams</v-chip>
+        <v-chip color="pink accent-2" text-color="white">{{numOfTutors}} Tutors</v-chip>
+      </v-layout>
     </v-card-actions>
   </v-card>
 </template>
@@ -28,6 +30,7 @@
 <script>
   export default {
     name: 'dashboard-list-item',
+    props: ['subjectId', 'title', 'assessmentDate', 'numOfSeats', 'numOfTutors', 'numOfExams', 'image'],
   };
 </script>
 
@@ -44,6 +47,10 @@
     font-weight: bold;
     font-size: 30px;
     background-color: rgba(255, 255, 255, 0.7);
+  }
+
+  .card-actions {
+    font-weight: bold;
   }
 
 </style>
