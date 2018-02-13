26b6c950f07101fdde101f69cef8b9dcaf551203
Alberto Arias <alberto@boodland.com>
2018-02-13 13:59:30 +0100

    Create brand new SubjectContainer component

---
+++ src/components/subject/SubjectContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <subject-search></subject-search>
+    <subject-list></subject-list>
+  </div>
+</template>
+
+<script>
+  import SubjectSearch from './SubjectSearch';
+  import SubjectList from './SubjectList';
+
+  export default {
+    name: 'subject-container',
+    components: {
+      SubjectSearch,
+      SubjectList,
+    },
+  };
+</script>
+
