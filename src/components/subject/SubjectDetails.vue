289ed72bce68ee5fcc6d8a9a97d7420723d11daa
Alberto Arias <alberto@boodland.com>
2018-02-15 21:16:08 +0100

    Start creation brand new subject-details component

---
+++ src/components/subject/SubjectDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-content>
+      <div slot="header">
+      </div>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
