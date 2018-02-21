35e2f03287645b37a2521561d272f764c3f02810
Alberto Arias <alberto@boodland.com>
2018-02-21 22:06:34 +0100

    Create brand new AppLoader component

---
+++ src/AppLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-progress-linear class="progress-loader" :indeterminate="true"></v-progress-linear>
+</template>
+
+<script>
+  export default {
+    name: 'app-loader',
+  };
+</script>
+
+<style scoped>
+  .progress-loader {
+    position: absolute;
+    opacity: 1;
+    margin: 0;
+    z-index: 4;
+  }
+</style>
+
+
