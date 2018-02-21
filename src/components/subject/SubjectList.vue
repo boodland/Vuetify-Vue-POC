58dad21f223babde6d2b0f82c80548aa89cf5bbc
Alberto Arias <alberto@boodland.com>
2018-02-21 14:27:57 +0100

    Get subject items using SubjectService Navigate to SubjectDetails component when clicking in the list item

--- src/components/subject/SubjectList.vue
+++ src/components/subject/SubjectList.vue
@@ -2,10 +2,13 @@
   <v-list two-line class="grey lighten-5">
     <v-layout row wrap justify-space-around>
       <v-flex class="py-1" xs12 md6 v-for="subjectItem in subjectList" :key="subjectItem.subjectId">
-        <v-list-tile>
+        <v-list-tile :to="'/subjects/' + subjectItem.subjectId">
           <img class="subject-avatar" :src="'/static/img/' + subjectItem.image + '.jpg'">
           <v-list-tile-content class="pl-3">
-            <v-list-tile-title v-highlight-term="filterTerms.title" class="subject-title">{{ subjectItem.title }}</v-list-tile-title>
+            <v-list-tile-title
+              v-highlight-term="filterTerms.title"
+              class="subject-title">{{ subjectItem.title }}
+            </v-list-tile-title>
             <v-list-tile-sub-title>
               <v-icon size="18px" class="black--text">event</v-icon>
               <span class="black--text subject-assessment-date">{{ subjectItem.assessmentDate.toDateString() }}</span>
@@ -27,80 +30,9 @@
 
 
 <script>
-  const subjectItems = [
-    {
-      subjectId: 1,
-      title: 'History',
-      assessmentDate: new Date(2018, 4),
-      numOfSeats: 680,
-      type: 'A',
-      color: 'red',
-      image: 'history',
-    },
-    {
-      subjectId: 2,
-      title: 'Bakery',
-      assessmentDate: new Date(2018, 0),
-      numOfSeats: 80,
-      type: 'B',
-      color: 'blue',
-      image: 'bakery',
-    },
-    {
-      subjectId: 3,
-      title: 'Law',
-      assessmentDate: new Date(2017, 9),
-      numOfSeats: 320,
-      type: 'C',
-      color: 'gold',
-      image: 'law',
-    },
-    {
-      subjectId: 4,
-      title: 'Science',
-      assessmentDate: new Date(2018, 10),
-      numOfSeats: 430,
-      type: 'D',
-      color: 'green',
-      image: 'science',
-    },
-    {
-      subjectId: 5,
-      title: 'Veggie Cuisine',
-      assessmentDate: new Date(2018, 2),
-      numOfSeats: 75,
-      type: 'B',
-      color: 'blue',
-      image: 'vegetables',
-    },
-    {
-      subjectId: 6,
-      title: 'Geography',
-      assessmentDate: new Date(2018, 6),
-      numOfSeats: 430,
-      type: 'A',
-      color: 'red',
-      image: 'geography',
-    },
-    {
-      subjectId: 7,
-      title: 'Algebra',
-      assessmentDate: new Date(2018, 3),
-      numOfSeats: 930,
-      type: 'D',
-      color: 'green',
-      image: 'algebra',
-    },
-    {
-      subjectId: 8,
-      title: 'Music',
-      assessmentDate: new Date(2018, 8),
-      numOfSeats: 30,
-      type: 'A',
-      color: 'red',
-      image: 'music',
-    },
-  ];
+  import SubjectService from './SubjectService';
+
+  const subjectItems = SubjectService.getSubjects();
 
   export default {
     name: 'subject-list',
