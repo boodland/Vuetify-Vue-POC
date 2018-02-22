2375f208fa7233938105cc069bc5b235a3d0608e
Alberto Arias <alberto@boodland.com>
2018-02-22 14:41:33 +0100

    Add transition component and transition styles

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
       <app-loader v-show="$root.loading"></app-loader>
       <v-container class="app-layout px-0 py-0">
         <app-dialog :message="$root.dialog.message"></app-dialog>
-        <router-view></router-view>
+        <transition :name="$root.transitionName">
+          <router-view class="main-view"></router-view>
+        </transition>
       </v-container>
     </v-content>
   </v-app>
@@ -33,6 +35,22 @@
 
   .app-grey-text {
     color: rgba(0,0,0,.54);
+  }
+
+  .main-view {
+    position: absolute;
+    transition: all .5s cubic-bezier(.55,0,.1,1);
+  }
+
+  .slide-left-enter, .slide-right-leave-active {
+    opacity: 0;
+    -webkit-transform: translate(100%, 0);
+    transform: translate(100%, 0);
+  }
+  .slide-left-leave-active, .slide-right-enter {
+    opacity: 0;
+    -webkit-transform: translate(-100%, 0);
+    transform: translate(-100%, 0);
   }
 
   @media (min-width: 900px){
