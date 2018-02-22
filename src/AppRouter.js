57f4a7fa1256076fab8008b601e0b5e5f5635923
Alberto Arias <alberto@boodland.com>
2018-02-22 14:40:33 +0100

    Add dynamic transition logic and refactor router

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -4,47 +4,56 @@ import DashboardList from '@/components/dashboard/DashboardList';
 import SubjectRoutes from '@/components/subject/SubjectRouter';
 import TutorRoutes from '@/components/tutor/TutorRouter';
 
-const createRouter = () => {
-  const router = new Router({
-    mode: 'history',
-    routes: [
-      {
-        path: '/dashboard',
-        name: 'DashboardList',
-        component: DashboardList,
-      },
-      ...SubjectRoutes,
-      ...TutorRoutes,
-      {
-        path: '/exams',
-        meta: { requiresAuth: true, message: 'exams list' },
-      },
-      { path: '*', redirect: '/dashboard' },
-    ],
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'DashboardList',
+      component: DashboardList,
+    },
+    ...SubjectRoutes,
+    ...TutorRoutes,
+    {
+      path: '/exams',
+      meta: { requiresAuth: true, message: 'exams list' },
+    },
+    { path: '*', redirect: '/dashboard' },
+  ],
+});
+
+const setGuards = () => {
+  router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+      router.app.dialog.message = `navigate to ${to.meta.message}`;
+      router.app.dialog.show = true;
+      next(false);
+    } else {
+      router.app.loading = true;
+      next();
+    }
   });
-  return router;
+
+  router.afterEach((to, from) => {
+    router.app.loading = false;
+    if (to.name === 'DashboardList') {
+      router.app.transitionName = 'slide-left';
+    } else if (from.name === 'DashboardList') {
+      router.app.transitionName = 'slide-right';
+    } else {
+      const toDepth = to.path.split('/').length;
+      const fromDepth = from.path.split('/').length;
+      router.app.transitionName = toDepth > fromDepth ? 'slide-right' : 'slide-left';
+    }
+  });
 };
 
 class AppRouter {
   static setVueConfiguration() {
     Vue.use(Router);
 
-    const router = createRouter();
-
-    router.beforeEach((to, from, next) => {
-      if (to.matched.some(record => record.meta.requiresAuth)) {
-        router.app.dialog.message = `navigate to ${to.meta.message}`;
-        router.app.dialog.show = true;
-        next(false);
-      } else {
-        router.app.loading = true;
-        next();
-      }
-    });
-
-    router.afterEach(() => {
-      router.app.loading = false;
-    });
+    setGuards();
 
     return router;
   }
